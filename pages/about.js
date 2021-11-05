3accd9ac915c3c0997bc5fa35ff1cf4d84c093a8
shinySonic <mail.eduard.schwarzkopf@gmail.com>
2021-11-05 08:49:00 +0100

    cleanup

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,11 @@
 import Container from "../components/container";
-import Intro from "../components/intro";
 import Layout from "../components/layout";
-import Head from "next/head";
-import { CMS_NAME } from "../lib/constants";
 
 export default function About() {
   return (
     <>
       <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
         <Container>
-          <Intro />
         </Container>
       </Layout>
     </>
