37e26468e9f598fea8714840a4c3f03e51f37807
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-11-06 06:14:38 +0100

    more cleanups

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,7 @@
-import Container from './container'
-
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        {/* TODO: Add content here */}
-      </Container>
+      {/* TODO: Add content here */}
     </footer>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,11 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
+import Layout from "../components/layout";
 
-export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
   return (
     <>
       <Layout>
-        <Container>
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
+        <div>AAAAAAAAAAAAAAAAAAAAAaa</div>
       </Layout>
     </>
-  )
-}
-
-export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
+  );
 }
