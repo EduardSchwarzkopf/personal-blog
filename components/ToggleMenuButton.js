424f559e56dfdc44e994e72e1ed80ffa766b4332
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-11-20 19:21:18 +0100

    layout hotfix

--- components/ToggleMenuButton.js
+++ components/ToggleMenuButton.js
@@ -4,13 +4,15 @@ import { useNavigation } from "./Providers/NavigationContext";
 export default function ToggleMenuButton() {
   const { isOpen, toggleMenu } = useNavigation();
   return (
-    <div className="flex items-center space-x-3">
-      <button
-        onClick={toggleMenu}
-        className="flex items-center justify-center p-2 rounded-md cursor-pointer lg:hidden hover:bg-gray-200"
-      >
-        {isOpen ? <TimesIcon /> : <BurgerMenuIcon />}
-      </button>
+    <div className="sticky md:hidden top-0 z-10 flex flex-col px-3 py-2 bg-white bg-opacity-90 filter-blur">
+      <div className="flex items-center space-x-3">
+        <button
+          onClick={toggleMenu}
+          className="flex items-center justify-center p-2 rounded-md cursor-pointer lg:hidden hover:bg-gray-200"
+        >
+          {isOpen ? <TimesIcon /> : <BurgerMenuIcon />}
+        </button>
+      </div>
     </div>
   );
 }
--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,11 @@ import { NavigtionProvider } from "./Providers/NavigationContext";
 import ToggleMenuButton from "./ToggleMenuButton";
 
 export default function Layout({ children, list, label }) {
+  const mainContent = (
+    <div className="w-full max-w-3xl px-4 py-8 pb-10 mx-auto md:px-8">
+      {children}
+    </div>
+  );
   return (
     <>
       <Meta />
@@ -15,18 +20,19 @@ export default function Layout({ children, list, label }) {
           <div className="flex w-full h-full min-h-screen">
             <div className="flex flex-1">
               <div className="flex w-full">
-                <main className="relative flex flex-col w-full max-h-screen overflow-y-auto bg-white">
-                  {list ? (
+                {list ? (
+                  <>
                     <ListContainer list={list} label={label} />
-                  ) : (
-                    <div className="sticky top-0 z-10 flex flex-col px-3 py-2 bg-white bg-opacity-90 filter-blur">
+                    {mainContent}
+                  </>
+                ) : (
+                  <>
+                    <div className="relative flex flex-col w-full max-h-screen overflow-y-auto bg-white">
                       <ToggleMenuButton />
+                      {mainContent}
                     </div>
-                  )}
-                  <div className="w-full max-w-3xl px-4 py-8 pb-10 mx-auto md:px-8">
-                    {children}
-                  </div>
-                </main>
+                  </>
+                )}
               </div>
             </div>
             <Footer />
