3627f357f823cfcccd2995ae09adbebe3d485e14
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-08 07:42:56 +0100

    added matomo tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/index.css'
+import "../styles/index.css";
+
+import { init } from "@socialgouv/matomo-next";
+import { useEffect } from "react";
+
+const MATOMO_URL = "https://analytics.schwarzkopf.center/";
+const MATOMO_SITE_ID = 1;
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
+  return <Component {...pageProps} />;
 }
