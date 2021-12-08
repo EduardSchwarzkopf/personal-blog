9663f6ec67554c712390be4d0c4bea80bd3fcd05
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-08 07:08:20 +0100

    added rounded style

--- components/VideoFile.js
+++ components/VideoFile.js
@@ -1,6 +1,6 @@
 export function VideoFile({ filePath }) {
   return (
-    <video autoPlay loop muted>
+    <video className="rounded-xl" autoPlay loop muted>
       <source type="video/mp4" src={filePath + ".mp4"}></source>
       <source type="video/webm" src={filePath + ".webm"}></source>
       <p>Your browser does not support the video element.</p>
