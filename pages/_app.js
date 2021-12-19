01974e9dfd331b4ca1eb69ee52d23a84c5df6e08
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-12-19 07:05:22 +0100

    matomo url fix

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "../styles/index.css";
 import { init } from "@socialgouv/matomo-next";
 import { useEffect } from "react";
 
-const MATOMO_URL = "https://analytics.schwarzkopf.center/";
+const MATOMO_URL = "https://analytics.schwarzkopf.center";
 const MATOMO_SITE_ID = 1;
 
 export default function MyApp({ Component, pageProps }) {
