bf21b6c36ee0e53806dc292eccdff60436ade4c7
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-13 21:16:11 +0100

    branding removed

--- components/sidebar.js
+++ components/sidebar.js
@@ -104,18 +104,6 @@ export default function SidebarNavigation() {
           return <NavigationLink key={i} link={link} />;
         })}
       </ul>
-      <div className="text-center text-xs text-accents-3 mb-5">
-        <p>
-          powered by{" "}
-          <a
-            href="https://github.com/brianlovin/briOS"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            briOS
-          </a>
-        </p>
-      </div>
     </nav>
   );
 }
